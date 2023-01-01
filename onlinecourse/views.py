6e6cfd40e8bf493ebc9448602afb9f0f6bdd6411
camperjx <camper.jxhuang@gmail.com>
2023-01-02 01:09:21 +1000

    Update views.py

--- onlinecourse/views.py
+++ onlinecourse/views.py
@@ -146,7 +146,6 @@ def extract_answers(request):
         # For each selected choice, check if it is a correct answer or not
         # Calculate the total score
 def show_exam_result(request, course_id, submission_id):
-<<<<<<< HEAD
     # course_id=request.GET.get("courseid")
     # submission_id=request.GET.get("submissionid")
     context = {}
@@ -162,12 +161,3 @@ def show_exam_result(request, course_id, submission_id):
     context['choices'] = choices
     return render(request, 'onlinecourse/exam_result_bootstrap.html', context)
     
-=======
-    course = get_object_or_404(Course, pk=course_id)
-    submission = get_object_or_404(Course, pk=submission_id)
-    choice_ids = submission.chocies
-    return redirect('onlinecourse:index')
-
-
-
->>>>>>> 705c512a7f89efa6d5cafc4c60bc4746abccdc55
